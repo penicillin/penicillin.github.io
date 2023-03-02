07187783bc40e828bf1bdd5fb61ff5e189b50191
penicillin <www.xx.com@163.com>
2023-03-02 13:48:02 +0800

    Add files via upload

--- WeChat/Users/setUser.js
+++ WeChat/Users/setUser.js
@@ -6,3 +6,12 @@ user_name_array.push('王明星');
 user_name_array.push('杨素坚');
 user_name_array.push('尤扬荣');
 user_name_array.push('叶小武');
+
+user_name_array.push('-');
+
+user_name_array.push('陈木清 亲属');
+user_name_array.push('褚立军 亲属');
+user_name_array.push('王明星 亲属');
+user_name_array.push('杨素坚 亲属');
+user_name_array.push('尤扬荣 亲属');
+user_name_array.push('叶小武 亲属');
